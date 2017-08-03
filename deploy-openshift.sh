aaca5cb03871ec106501523f5014d03854a2f875
Steve Mulholland <smulholland@ukcloud.com>
2017-08-03 18:35:36 +0100

    adding logging to haproxy and moving to template approach

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -3,6 +3,6 @@
 
 ansible-playbook --private-key ../id_rsa_jenkins -i localhost initalise.yaml -vvv
 ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts setup_keepalived.yaml
-ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts post_config_haproxy.yaml
+ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts setup_haproxy.yaml
 ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
 ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts setup_storage_classes.yml
