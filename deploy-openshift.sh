07280de335dab59afcb23025be9e0e7cfd31780c
Cloud User <cloud-user@bastion-host.localdomain>
2017-08-31 05:40:39 +0000

    Corrected paths

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-
-ansible-playbook --private-key ../id_rsa_jenkins -i localhost initalise.yaml -vvv
-ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts setup_keepalived.yaml
-ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts setup_haproxy.yaml
-ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
-ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts setup_storage_classes.yml
+ansible-playbook --private-key ../id_rsa_jenkins -i localhost bastion.yml
+ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts site.yml
