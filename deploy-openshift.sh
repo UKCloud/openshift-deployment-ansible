4d683fe1b82158f0c977b6a0945a6d0d776dc913
Steve Mulholland <smulholland@ukcloud.com>
2017-09-18 22:19:15 +0100

    making files paths use home directory rather than relative paths

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts bastion.yml
-ansible-playbook --private-key ../id_rsa_jenkins -i openshift-ansible-hosts site.yml
+ansible-playbook --private-key ~/id_rsa_jenkins -i openshift-ansible-hosts bastion.yml
+ansible-playbook --private-key ~/id_rsa_jenkins -i openshift-ansible-hosts site.yml
