6db01cddb112b98bfbbc9e2319022500ebb12631
Andy Watkins <awatkins@ukcloud.com>
2017-11-01 10:35:07 +0000

    specify local connection for bastion/initialisation play

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # build the openshift-ansible-hosts file for use in the next play.
-ansible-playbook -i localhost, bastion.yml
+ansible-playbook -i localhost, -c local bastion.yml
 
 ansible-playbook --private-key ~/id_rsa_jenkins -i openshift-ansible-hosts site.yml
