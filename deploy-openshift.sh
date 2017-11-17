f5b313affa450587111f611dc4a89dc94a3fb089
Gareth Ellner <gellner@ukcloud.com>
2017-11-17 17:18:32 +0000

    added RH SR number to deploy-openshift.sh

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Temporary: Patch the openshift-ansible install_cassandra.yaml for version 3.6.173.0.48-1
+# RH support case 01975215 is open for this. Version in the grep may need to be updated
+# if another broken version is pushed to the repo
 yum list openshift-ansible 2>&1 | grep 3.6.173.0.48-1 >/dev/null
 if [ $? -eq 0 ]; then
 	curl https://raw.githubusercontent.com/openshift/openshift-ansible/80d141b5d60da9afbd3c02350933c090d1839c46/roles/openshift_metrics/tasks/install_cassandra.yaml > /tmp/install_cassandra.yaml
