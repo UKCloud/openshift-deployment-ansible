f6297b699d719ea2d4bb9f0425f492d7cb1312bc
Andy Watkins <awatkins@ukcloud.com>
2017-11-30 12:09:26 +0000

    Downgrade the broken Ansible install.

--- deploy-openshift.sh
+++ deploy-openshift.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ -n $(ansible --version | grep 2.4.1.0) ]] ; then
+    echo "Downgrading from non-working Ansible version."
+    sudo yum remove -y ansible-2.4.1.0-1.el7.noarch
+    sudo yum install -y ansible-2.4.0.0-5.el7.noarch atomic-openshift-utils atomic-openshift-excluder atomic-openshift-clients
+fi
+
+
 # build the openshift-ansible-hosts file for use in the next play.
 ansible-playbook -i localhost, -c local bastion.yml
 
