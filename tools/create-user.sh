6331c57c6c31bc55492f9f7cd743e1c315eaa9e4
Andy Watkins <andy.n.watkins@gmail.com>
2017-12-13 09:52:40 +0000

    Feature/i41 create user (#43)
    * It is not possible to call a playbook within a task. Switch to calling create-user, as it's cleaner
    
    * Tidied up create-user

--- tools/create-user.sh
+++ tools/create-user.sh
@@ -8,10 +8,15 @@ if [[ -z $username ]] || [[ -z $password ]]; then
   echo -e "help: This script will add users to htpasswd for OpenShift.\n\n"
   echo -e "usage: $0 <username> <password>\n"
   exit 1
-else
-  if [[ $debug != 0 ]]; then
-    ansible-playbook --private-key ~/id_rsa_jenkins -i ../openshift-ansible-hosts -e USERNAME=$username -e PASSWORD=$password playbooks/htpassword.yaml -vv
-  else
-    ansible-playbook --private-key ~/id_rsa_jenkins -i ../openshift-ansible-hosts -e USERNAME=$username -e PASSWORD=$password playbooks/htpassword.yaml
-  fi
 fi
+
+privkey=~/id_rsa_jenkins
+inventory=../openshift-ansible-hosts
+playbook=playbooks/htpassword.yaml
+extras=""
+
+if [[ $debug != 0 ]]; then
+    extras="-vv"
+fi
+
+ansible-playbook --private-key $privkey -i $inventory -e USERNAME=$username -e PASSWORD=$password $extras $playbook
