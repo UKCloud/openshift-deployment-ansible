96ceb986fe8f2fda5b2ebb7fda8dc794a14b8477
Andy Watkins <awatkins@ukcloud.com>
2018-01-17 10:16:47 +0000

    converted upgrade tasks into a role

--- deploy-with-upgrade.sh
+++ deploy-with-upgrade.sh
@@ -5,9 +5,6 @@
 # If called with no args, the initial user is assumed to be "admin",
 # and a password is generated.
 #
-# Command-line options
-#    -u   if present, upgrade and reboot each node during deployment.
-#
 # Optional args: (intended to allow the test pipeline to supply known credentials)
 #    $1 - username of the admin user
 #    $2 - password for the admin user
@@ -19,36 +16,11 @@
 # All optional args are positional.
 #
 # TODO: merge with deploy-openshift.sh
-#       consider adding it to the pre-installation role
-
-usage() {
-    echo "Usage: $0 [-u] [user1 password1 [user2 password2]]"
-    echo ""
-    echo "-u causes nodes to be upgraded and rebooted"
-    exit 1;
-}
-
 
 PRIVKEY_ARG=''
 if [[ -f ~/id_rsa_jenkins ]] ; then
     PRIVKEY_ARG='--private-key ~/id_rsa_jenkins'
 fi
-
-
-# Arg Parsing
-DO_UPGRADES="NO"
-while getopts "u" o; do
-    case "${o}" in
-        u)
-            echo "arg U"
-            DO_UPGRADES="YES"
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
-shift $((OPTIND-1))
 
 if [[ "$1" == "" && "$2" == "" ]]; then
     ADMIN_USER="admin"
@@ -60,6 +32,7 @@ fi
 
 
 # Store the password
+echo "SHELL [store and encrypt admin password] ..."
 echo "${ADMIN_USER}:${ADMIN_PASSWORD}" > /home/cloud-user/passwords.txt;
 
 # Turn the plain-text password into an htpasswd password
@@ -73,27 +46,21 @@ rm tmp_htpasswd
 # and install bind-utils
 ansible-playbook -i localhost, -c local bastion.yml
 
-# Upgrade nodes and loadbalancers.
-if [[ "$DO_UPGRADES" = "YES" ]]; then
-    ansible-playbook $PRIVKEY_ARG -i openshift-ansible-hosts upgrade_and_reboot_servers.yml
-fi
-
 # Perform the deployment
 ansible-playbook $PRIVKEY_ARG -i openshift-ansible-hosts site.yml
 
 # Create an additional user if $3 and $4 are supplied
 if [[ "$3" != "" && "$4" != "" ]]; then
+    echo "SHELL [create secondary user] ..."
     (cd tools; ./create-user.sh "$3" "$4")
     echo "$3:$4" >> /home/cloud-user/passwords.txt
 fi
 
 # Upgrade the Bastion Host
-if [[ "$DO_UPGRADES" = "YES" ]]; then
+if [[ -n $(grep doUpgrades group_vars/all.yml |grep True) ]]; then
     echo ""
-    echo "UPGRADING Bastion... (DO NOT interrupt)"
-    ansible -i localhost, localhost  -m yum -b -a 'name=* state=latest'
-
-    echo "Please reboot the bastion host as soon as practical with the following command:"
+    echo "Please reboot the bastion host as soon as practical with the following:"
     echo "     sudo reboot --reboot"
+    echo ""
 
 fi
