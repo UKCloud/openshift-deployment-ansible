dda357b32fff222d24d14400251f0cf46e5d9cfe
Andy Watkins <awatkins@ukcloud.com>
2018-01-22 10:37:10 +0000

    removed heat-specific comment

--- sanity_tests.sh
+++ sanity_tests.sh
@@ -7,7 +7,6 @@ function do_we_have_ip_addresses {
        grep ${node} ${GROUP_VARS_FILE} | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" > /dev/null
        if [[ $? -ne 0 ]]; then
            echo "ERROR: ${node} does not have a valid IP address."
-           echo "(This can be caused if you have another stack in your tenancy, stealing your IP Addresses.)"
            exit 1
        fi
    done
