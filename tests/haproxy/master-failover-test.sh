399c05ccc0c782dc8dbe96be766bc4b4fe042f46
kieran oneill <koneill@rhel74.local>
2018-02-05 15:30:58 +0000

    Playbooks for tests moved into relevant test folders and comments added to playbooks. Minor changes to absolute path names in bash script too.

--- tests/haproxy/master-failover-test.sh
+++ tests/haproxy/master-failover-test.sh
@@ -11,7 +11,7 @@ echo
 # Creates symbolic link to ansible config file in order to send log output to ~/ansible.log
 if [[ ! -L ansible.cfg ]]
 then
-	ln -s /usr/share/ansible/openshift-deployment-ansible/ansible.cfg ansible.cfg
+	ln -s ../../ansible.cfg ansible.cfg
 fi
 
 # Checks the master nodes are in state "Ready" and exits script if not. If they are it runs the ansible playbooks to poll the API and hard reboot the master nodes in turn.
@@ -21,22 +21,22 @@ then
 	echo
 	exit
 else
-	ansible-playbook /usr/share/ansible/openshift-deployment-ansible/poll.yml > /dev/null 2>&1 &
+	ansible-playbook poll.yml > /dev/null 2>&1 &
 	sleep 2
-	ansible-playbook /usr/share/ansible/openshift-deployment-ansible/node_rotate.yml > /dev/null 2>&1 & 
+	ansible-playbook node_rotate.yml > /dev/null 2>&1 & 
 fi
 
 # Loop to wait until the node_rotate playbook is finished and allow the poll playbook to be killed once it is.
-until [[ -z $(ps -ef | grep "/[u]sr/bin/ansible-playbook /usr/share/ansible/openshift-deployment-ansible/node_rotate.yml" | awk '{ print $2}') ]]
+until [[ -z $(ps -ef | grep "/[u]sr/bin/ansible-playbook node_rotate.yml" | awk '{ print $2}') ]]
 do
 	sleep 1
 done
 
 # Writes output of poll playbook to haproxytest.log determines this from the playbook PID.
-cat ~/ansible.log | grep $(ps -ef | grep "/[u]sr/bin/ansible-playbook /usr/share/ansible/openshift-deployment-ansible/poll.yml" | awk '{ print $2}' | head -1) > haproxytest.log
+cat ~/ansible.log | grep $(ps -ef | grep "/[u]sr/bin/ansible-playbook poll.yml" | awk '{ print $2}' | head -1) > haproxytest.log
 
 #Kills poll playbook.
-ps -ef | grep "/[u]sr/bin/ansible-playbook /usr/share/ansible/openshift-deployment-ansible/poll.yml" | awk '{ print $2}' | xargs -x kill -9
+ps -ef | grep "/[u]sr/bin/ansible-playbook poll.yml" | awk '{ print $2}' | xargs -x kill -9
 
 #Uses regex to filter haproxytest.log for the information needed and then write it back to the log.
 perl -ne 'print if /item=\d/' haproxytest.log | awk '{ print $1 " " $2 " connection " $6}' > connect.log
