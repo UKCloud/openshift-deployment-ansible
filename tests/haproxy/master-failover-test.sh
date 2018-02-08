447beecdcfd160d6559b7a564caa72e243334eef
kieran oneill <koneill@rhel74.local>
2018-02-08 14:24:18 +0000

    Fixed scripts to work over SSH on bastion in pipeline.

--- tests/haproxy/master-failover-test.sh
+++ tests/haproxy/master-failover-test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Help with debugging pipeline issues
-pwd
-whoami
 
+hostname
 pwd
 
 # Logging into openshift in order to run oc commands later in script.
@@ -20,11 +19,11 @@ master2state=$(oc get node | grep master-2 | awk '{ print $2 }' | cut -d , -f 1)
 echo Script starting this will take around 2 minutes to complete.
 echo
 
-# Disabled for now as testing creating config file in haproxy directory {Creates symbolic link to ansible config file in order to send log output to ~/ansible.log}
-#if [[ ! -L ansible.cfg ]]
-#then
-#	ln -s ../../ansible.cfg ansible.cfg
-#fi
+# Creates symbolic link to ansible config file in order to send log output to ~/ansible.log
+if [[ ! -L ansible.cfg ]]
+then
+	ln -s ../../ansible.cfg ansible.cfg
+fi
 
 # Checks the master nodes are in state "Ready" and exits script if not. If they are it runs the ansible playbooks to poll the API and hard reboot the master nodes in turn.
 if [[ $master0state != Ready ]] || [[ $master1state != Ready ]] || [[ $master2state != Ready ]]
@@ -33,27 +32,26 @@ then
 	echo
 	exit 1
 else
-	ansible-playbook /tmp/workspace/build-openshift-openshift-build-pipeline/openshift-deployment-ansible/tests/haproxy/poll.yml &
+	ansible-playbook poll.yml &
 	sleep 2
-	ansible-playbook /tmp/workspace/build-openshift-openshift-build-pipeline/openshift-deployment-ansible/tests/haproxy/node_rotate.yml & 
+	ansible-playbook node_rotate.yml & 
 fi
 
 # debugging purposes
-echo $(ps -ef | grep "/[u]sr/bin/ansible-playbook /tmp/workspace/build-openshift-openshift-build-pipeline/openshift-deployment-ansible/tests/haproxy/poll.yml")
-echo $(ps -ef | grep "/[u]sr/bin/ansible-playbook /tmp/workspace/build-openshift-openshift-build-pipeline/openshift-deployment-ansible/tests/haproxy/node_rotate.yml")
+echo $(ps -ef | grep "/[u]sr/bin/ansible-playbook poll.yml")
+echo $(ps -ef | grep "/[u]sr/bin/ansible-playbook node_rotate.yml")
 
 # Loop to wait until the node_rotate playbook is finished and allow the poll playbook to be killed once it is.
-until [[ -z $(ps -ef | grep "/[u]sr/bin/ansible-playbook /tmp/workspace/build-openshift-openshift-build-pipeline/openshift-deployment-ansible/tests/haproxy/node_rotate.yml" | awk '{ print $2}') ]]
+until [[ -z $(ps -ef | grep "/[u]sr/bin/ansible-playbook node_rotate.yml" | awk '{ print $2}') ]]
 do
 	sleep 1
 done
 
 # Writes output of poll playbook to haproxytest.log determines this from the playbook PID.
-cat /tmp/workspace/build-openshift-openshift-build-pipeline/openshift-deployment-ansible/tests/haproxy/ansible.log \
-| grep $(ps -ef | grep "/[u]sr/bin/ansible-playbook /tmp/workspace/build-openshift-openshift-build-pipeline/openshift-deployment-ansible/tests/haproxy/poll.yml" | awk '{ print $2}' | head -1) > haproxytest.log
+cat ~ansible.log | grep $(ps -ef | grep "/[u]sr/bin/ansible-playbook poll.yml" | awk '{ print $2}' | head -1) > haproxytest.log
 
 #Kills poll playbook.
-ps -ef | grep "/[u]sr/bin/ansible-playbook /tmp/workspace/build-openshift-openshift-build-pipeline/openshift-deployment-ansible/tests/haproxy/poll.yml" | awk '{ print $2}' | xargs -x kill -9
+ps -ef | grep "/[u]sr/bin/ansible-playbook poll.yml" | awk '{ print $2}' | xargs -x kill -9
 
 #Uses regex to filter haproxytest.log for the information needed and then write it back to the log.
 perl -ne 'print if /item=\d/' haproxytest.log | awk '{ print $1 " " $2 " connection " $6}' > connect.log
