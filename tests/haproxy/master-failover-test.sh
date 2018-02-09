c7a51b61c916859b6ac02b148e857371bfa146e0
koneill94 <35918022+koneill94@users.noreply.github.com>
2018-02-09 16:04:35 +0000

    Update master-failover-test.sh

--- tests/haproxy/master-failover-test.sh
+++ tests/haproxy/master-failover-test.sh
@@ -8,7 +8,7 @@ then
 fi
 
 # Logging into openshift in order to run oc commands later in script.
-oc login -u $1 -p $2 --server="https://ocp.$(cat /usr/share/ansible/openshift-deployment-ansible/group_vars/all.yml \
+oc login -u $1 -p $2 --server="https://ocp.$(cat ../../group_vars/all.yml \
 | grep domainSuffix | awk '{print $2}'):8443" --insecure-skip-tls-verify
 
 oc project default
